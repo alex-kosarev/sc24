d78ad2cb6c87870ad88f94b336cc7b1feaa99405
Alexander Kosarev <inbox@alexkosarev.name>
2024-02-19 13:13:57 +0500

    SC24EP13

--- admin-server/src/main/java/ag/selm/admin/config/SecurityBeans.java
+++ admin-server/src/main/java/ag/selm/admin/config/SecurityBeans.java
@@ -5,6 +5,7 @@ import jakarta.annotation.Priority;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -34,10 +35,15 @@ public class SecurityBeans {
     @Priority(0)
     public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .securityMatcher(request -> Optional.ofNullable(request.getHeader(HttpHeaders.AUTHORIZATION))
-                        .map(header -> header.startsWith("Bearer ")).orElse(false))
+                .securityMatchers(customizer -> customizer
+                        .requestMatchers(HttpMethod.POST, "/instances")
+                        .requestMatchers(HttpMethod.DELETE, "/instances/*")
+                        .requestMatchers("/actuator/**"))
                 .oauth2ResourceServer(customizer -> customizer.jwt(Customizer.withDefaults()))
-                .authorizeHttpRequests(customizer -> customizer.anyRequest().hasAuthority("SCOPE_metrics_server"))
+                .authorizeHttpRequests(customizer -> customizer.requestMatchers("/instances", "/instances/*")
+                        .hasAuthority("SCOPE_metrics_server")
+                        .requestMatchers("/actuator/**").hasAuthority("SCOPE_metrics")
+                        .anyRequest().denyAll())
                 .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(CsrfConfigurer::disable)
                 .build();
